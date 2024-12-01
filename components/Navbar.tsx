0e9383fbae48b0a89a70904a4b017e9e853afa93
Sachin Kumar <stdtech7878@gmail.com>
2024-12-01 20:40:17 +0530

    app name is changed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ import MobileNav from './MobileNav';
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-      <Link href="/" className="flex items-center gap-1">
+      <Link href="/" className="flex items-center gap-2">
         <Image
           src="/icons/yoom-logo.png"
           width={32}
@@ -16,7 +16,7 @@ const Navbar = () => {
           className="max-sm:size-10"
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          YOOM
+          NEXEET
         </p>
       </Link>
       <div className="flex-between gap-5">
